c632b5892cc0722ed65b9f6708fd17b56634b830
Aliaksei <alexx333by@gmail.com>
2018-09-26 00:14:47 +0300

    Implemented Simpsons integrate method

--- GeneratorUsageExample/Example.cs
+++ GeneratorUsageExample/Example.cs
@@ -1,23 +1,60 @@
 ﻿using System;
 using System.IO;
 using System.Reflection;
-using MathNet.Numerics.Integration;
 
 namespace ProbabilityModelGenerator.Example
 {
     class Example
     {
+        public delegate double IntegratableFunctionCallback(double x);
+
+        private static double SimpsonsIntegrate(IntegratableFunctionCallback functionCallback, double leftBound, double rightBound, uint segments)
+        {
+            if (segments <= 0)
+            {
+                throw new ArgumentException("Segments should be positive number");
+            }
+            if (segments % 2 == 1)
+            {
+                throw new ArgumentException("Segments should be even");
+            }
+            if (rightBound < leftBound)
+            {
+                throw new ArgumentException("Right bound should be above left bound");
+            }
+
+            double sum = 0;
+            double step = (rightBound - leftBound) / segments;
+            double x = leftBound + step;
+
+            while (x < rightBound)
+            {
+                sum += 4 * functionCallback(x);
+                x += step;
+                if (x < rightBound)
+                {
+                    sum += 2 * functionCallback(x);
+                    x += step;
+                }
+            }
+
+            return (step / 3) * (sum + functionCallback(leftBound) + functionCallback(rightBound));
+        }
+
         private static double AlphaDensity(double x)
         {
-            const long a = 4, b = 20;
+            const int a = 1, b = 1;
 
             return (b / (Math.Pow(x, 2) * Math.Sqrt(2 * Math.PI)))
-                * Math.Pow(Math.E, -0.5 * Math.Pow(b / x - a, 2));
+                * Math.Exp(-0.5 * Math.Pow(b / x - a, 2));
         }
 
         private static double AlphaDistribution(double x)
         {
-            return NewtonCotesTrapeziumRule.IntegrateAdaptive(AlphaDensity, 0, x, x / 100);
+            const int segments = 10000;
+            const double leftBound = 0.001;
+
+            return SimpsonsIntegrate(AlphaDensity, leftBound, x, segments);
         }
 
         static void Main(string[] args)
@@ -46,7 +83,7 @@ namespace ProbabilityModelGenerator.Example
                 Console.WriteLine("Step should be floating point value");
                 return;
             }
-            if (!long.TryParse(args[0], out amount))
+            if (!long.TryParse(args[3], out amount))
             {
                 Console.WriteLine("Amount should be integer number");
                 return;
@@ -56,12 +93,16 @@ namespace ProbabilityModelGenerator.Example
             if (args.Length < 5)
             {
                 generator = new ProbabilityModelGenerator(AlphaDistribution);
+                generator.Generate(leftBound, rightBound, step, amount, true);
             }
             else
             {
                 generator = new ProbabilityModelGenerator(AlphaDistribution, new FileStream(args[4], FileMode.OpenOrCreate, FileAccess.Write));
+                generator.Generate(leftBound, rightBound, step, amount, false);
             }
-            generator.Generate(leftBound, rightBound, step, amount, true);
+
+            Console.WriteLine("Done.");
+            Console.ReadKey();
         }
     }
 }
